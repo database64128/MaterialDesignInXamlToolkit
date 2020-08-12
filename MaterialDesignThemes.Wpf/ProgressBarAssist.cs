b185b15ac53025d3dc90cb528d1f7f1cb2cba462
bombipappoo <bombipappoo@gmail.com>
2020-08-12 20:51:45 +0900

    IsIndeterminate test

---
+++ MaterialDesignThemes.Wpf/ProgressBarAssist.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace MaterialDesignThemes.Wpf
+{
+    public static class ProgressBarAssist
+    {
+        public static readonly DependencyProperty IsIndeterminateProperty = DependencyProperty.RegisterAttached(
+            "IsIndeterminate", typeof(bool), typeof(ProgressBarAssist), new FrameworkPropertyMetadata(default(bool)));
+        public static bool GetIsIndeterminate(DependencyObject element) => (bool)element.GetValue(IsIndeterminateProperty);
+        public static void SetIsIndeterminate(DependencyObject element, bool value) => element.SetValue(IsIndeterminateProperty, value);
+    }
+}
